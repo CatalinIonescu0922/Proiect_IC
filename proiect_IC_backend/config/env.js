5636b9ad11f7a863d957c74539b54b85db29be22
CatalinIonescu0922 <cata.ionescu2003@gmail.com>
2025-04-23 22:50:05 +0300

    adaugare

--- proiect_IC_backend/config/env.js
+++ proiect_IC_backend/config/env.js
@@ -1,12 +1,14 @@
 import dotenv from 'dotenv'
 dotenv.config();
-
+// env obj ia toate valorile din .env file 
+// trebuie importat in fiecare fisier pentru a putea fi folosit
 const config_obj = {
     port :process.env.MYSQL_PORT,
     db_name : process.env.MYSQL_DATABASE,
     db_user : process.env.MYSQL_USER,
     db_password : process.env.MYSQL_PASSWORD,
     front_end_url : process.env.FRONT_END_URL,
-    db_host : process.env.MYSQL_HOST
+    db_host : process.env.MYSQL_HOST,
+    backend_port : process.env.BACKEND_PORT
 }
 export default config_obj;
--- proiect_IC_backend/server.js
+++ proiect_IC_backend/server.js
@@ -2,12 +2,11 @@ import express from 'express'
 import db from './config/dbconnect.js'
 import config_obj from './config/env.js'
 const app=express();
-// const config = require('./config/env');
 
+app.get('/', (req,res)=>{
+   res.send("hello world");
+})
 
-console.log(config_obj.port)
-console.log(config_obj.front_end_url)
-
-const result = await db.pool.query(`Select * from Roles`);
-
-console.log(result)
+app.listen(config_obj.backend_port, ()=>{
+    console.log(`app listen on port ${config_obj.backend_port}`)
+})
